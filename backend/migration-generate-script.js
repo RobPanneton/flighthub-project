8158255cff1b25d82684cb998230c75a576b3dd8
RobPanneton <robert.panneton@gmail.com>
2024-05-10 15:28:22 -0400

    connect data source and fix migration-generate script

--- backend/migration-generate-script.js
+++ backend/migration-generate-script.js
@@ -1,14 +1,16 @@
-// migration-script.js
 const exec = require("child_process").exec;
-const migrationName = process.argv[2] || "defaultMigrationName";
+const args = process.argv.slice(2); // Gets all arguments passed to the script
+const migrationName = args[0] || "defaultMigrationName"; // Use the first argument as the migration name or default
 
-const command = `ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:generate -d ./src/ormconfig.ts src/migration/${migrationName}`;
+const command = `npx ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:generate -d ./src/ormconfig.ts src/migration/${migrationName}`;
+
+console.log(`Executing command: ${command}`); // Log the command to be executed
 
 exec(command, (error, stdout, stderr) => {
   if (error) {
-    console.error(`exec error: ${error}`);
+    console.error("Execution Error:", error);
+    console.error("STDERR:", stderr);
     return;
   }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+  console.log("STDOUT:", stdout);
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,15 @@
 import "reflect-metadata";
 import "dotenv/config";
+
 import app from "./app";
+import { AppDataSource } from "./ormconfig";
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
+    await AppDataSource.initialize();
+
     app.listen(+PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
