d8475b5ec5b69ececaae6581828477c61e4dd83b
RobPanneton <robert.panneton@gmail.com>
2024-05-10 15:52:22 -0400

    add starter files for api services + small change for migrations spelling

--- backend/migration-generate-script.js
+++ backend/migration-generate-script.js
@@ -2,7 +2,7 @@ const exec = require("child_process").exec;
 const args = process.argv.slice(2); // Gets all arguments passed to the script
 const migrationName = args[0] || "defaultMigrationName"; // Use the first argument as the migration name or default
 
-const command = `npx ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:generate -d ./src/ormconfig.ts src/migration/${migrationName}`;
+const command = `npx ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:generate -d ./src/ormconfig.ts src/migrations/${migrationName}`;
 
 console.log(`Executing command: ${command}`); // Log the command to be executed
 
