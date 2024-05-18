532e5660611ef1aa36732c70de5e99ef57aa750a
RobPanneton <robert.panneton@gmail.com>
2024-05-17 21:33:25 -0400

    connect routes to server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application } from "express";
-import airlineRoutes from "./api/airline/routes/airline.routes";
+import { airlineRouter } from "./api/airline/routes/airline.routes";
+import { airportRouter } from "./api/airport/routes/airport.routes";
+import { flightRouter } from "./api/flight/routes/flight.routes";
 
 const app: Application = express();
 app.use(express.json());
 
-app.use("/airlines", airlineRoutes);
+app.use("/airlines", airlineRouter);
+app.use("/airports", airportRouter);
+app.use("/flights", flightRouter);
 
 export default app;
