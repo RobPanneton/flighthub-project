0e5a63baa84343adaf7d677b4d29f3fc81a9053f
RobPanneton <robert.panneton@gmail.com>
2024-05-17 21:21:13 -0400

    connect airport api to app and change naming for airlines

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from "express";
-import airlineRoutes from "./api/airline/routes/airline.routes";
+import { airlineRouter } from "./api/airline/routes/airline.routes";
+import { airportRouter } from "./api/airport/routes/airport.routes";
 
 const app: Application = express();
 app.use(express.json());
 
-app.use("/airlines", airlineRoutes);
+app.use("/airlines", airlineRouter);
+app.use("/airports", airportRouter);
 
 export default app;
