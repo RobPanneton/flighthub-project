953c35eb3e06b66d7ba3079b26a2f04037dd5b23
RobPanneton <robert.panneton@gmail.com>
2024-05-21 19:30:06 -0400

    add flight controller, service, controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import { airlineRouter } from "./api/airline/routes/airline.routes";
 import { airportRouter } from "./api/airport/routes/airport.routes";
 import { flightRouter } from "./api/flight/routes/flight.routes";
+import { tripRouter } from "./api/trip/routes/trip.routes";
 
 const app: Application = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use("/airlines", airlineRouter);
 app.use("/airports", airportRouter);
 app.use("/flights", flightRouter);
+app.use("/trips", tripRouter);
 
 export default app;
