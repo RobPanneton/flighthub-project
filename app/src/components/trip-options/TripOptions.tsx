0cf18ee6e6a6286676801ce2d6d5c0ead39b99c9
RobPanneton <robert.panneton@gmail.com>
2024-05-26 17:13:00 -0400

    add styling for flight details

--- app/src/components/trip-options/TripOptions.tsx
+++ app/src/components/trip-options/TripOptions.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+
 import { useTripsContext } from "../../context/TripsContext";
+import { useAirlinesContext } from "../../context/AirlinesContext";
 import { RoundTrip, Trip } from "../../types/tripTypes";
 import { getRandomNumForKey } from "../../utils/varUtils";
 
 import styles from "./TripOptions.module.scss";
+import { Airline } from "../../types/airlineTypes";
 
 export const TripOptions: React.FC = () => {
   const { trips } = useTripsContext();
+  const { airlines } = useAirlinesContext();
 
   const numOfTrips = trips.length;
 
@@ -14,6 +18,15 @@ export const TripOptions: React.FC = () => {
   const handleSelect = () => 0;
 
   const isRoundTrip = (trip: Trip | RoundTrip): trip is RoundTrip => (trip as RoundTrip).returnFlight !== undefined;
+
+  const getDuration = (departureTime: Date, arrivalTime: Date) => {
+    const totalMinutes = Math.floor((arrivalTime.getTime() - departureTime.getTime()) / 60000);
+    const hours = String(Math.floor(totalMinutes / 60));
+    const minutes = String(totalMinutes % 60).padStart(2, "0");
+    return `${hours}h${minutes}`;
+  };
+
+  const getThreeLastChars = (s: string) => s.slice(-3);
 
   return (
     <div className={styles.tripsWrapper}>
@@ -52,32 +65,47 @@ export const TripOptions: React.FC = () => {
                   </div>
                 );
               } else {
-                return (
-                  <div key={`${index}-${getRandomNumForKey}`} className={styles.result}>
-                    <div className={styles.priceAndCTA}>
-                      <div className={styles.price}>
-                        <h3>CAD {trip.totalPrice}</h3>
-                        <span>Final total price (taxes included)</span>
-                      </div>
-                      <button type='button' onClick={handleSelect}>
-                        <span>SELECT</span> <span>{">"}</span>
-                      </button>
-                    </div>
-                    <div className={styles.info}>
-                      <div className={styles.airline}>{trip.flight.airline}</div>
-                      <div className={styles.timeAndAirport}>
-                        <div className={styles.infoRow}>
-                          <span>{trip.flight.departure_time}</span>
-                          <span>{trip.flight.departure_airport}</span>
+                const airline: Airline | undefined = airlines?.find((a) => a.code === trip.flight.airline);
+                console.log({ trip });
+                if (airline)
+                  return (
+                    <div key={`${index}-${getRandomNumForKey}`} className={styles.result}>
+                      <div className={styles.priceAndCTA}>
+                        <div className={styles.price}>
+                          <h3>CAD {trip.totalPrice}</h3>
+                          <span>Final total price (taxes included)</span>
                         </div>
-                        <div className={styles.infoRow}>
-                          <span>{trip.flight.arrival_time}</span>
-                          <span>{trip.flight.arrival_airport}</span>
+                        <button type='button' onClick={handleSelect}>
+                          <span>SELECT</span> <span>{">"}</span>
+                        </button>
+                      </div>
+                      <div className={styles.info}>
+                        <div className={styles.flightDetails}>
+                          <div className={styles.logoContainer}>
+                            <img src={airline.logo} alt='airline-logo' />
+                          </div>
+                          <div className={styles.description}>
+                            <span>{airline.name}</span>
+                            <span className={styles.flightNum}>Flight {getThreeLastChars(trip.flight.number)}</span>
+                            <span className={styles.flightClass}>Economy</span>
+                            <span className={styles.flightDuration}>
+                              {getDuration(new Date(trip.flight.departure_time), new Date(trip.flight.arrival_time))}
+                            </span>
+                          </div>
+                        </div>
+                        <div className={styles.timeAndAirport}>
+                          <div className={styles.infoRow}>
+                            <span>{trip.flight.departure_time}</span>
+                            <span>{trip.flight.departure_airport}</span>
+                          </div>
+                          <div className={styles.infoRow}>
+                            <span>{trip.flight.arrival_time}</span>
+                            <span>{trip.flight.arrival_airport}</span>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
+                  );
               }
             })}
           </div>
